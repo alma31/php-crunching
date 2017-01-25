8a0761dee846542d9b126a3829b6e569d718b331
alma31 <lenni31@neuf.fr>
2017-01-25 10:48:15 +0100

    top 10 fini

--- php-crunching/films.php
+++ php-crunching/films.php
@@ -5,6 +5,22 @@
 	<title>php crunching 2</title>
 </head>
 <body>
+	<h1 style="text-align: center;">Top 100 films Ituines</h1>
+</br>
+<?php 
+
+$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
+$brut = json_decode($string, true);
+$top = $brut["feed"]["entry"]; # liste de films
+
+foreach ($top as $key => $value) {
 	
+	if ($key <= 10){
+		echo ($value['im:name']['label']."</br>");
+	}
+
+}
+?>
+
 </body>
 </html>
